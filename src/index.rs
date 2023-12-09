6ba3bc31a8e6b40529ef21c055b1fe5a9d646922
Jayce Fayne <jayce.fayne@mailbox.org>
2023-12-09 11:09:24 +0100

    index with index

--- src/index.rs
+++ src/index.rs
@@ -5,14 +5,14 @@ impl<K, V> Index<usize> for VecBTreeMap<K, V> {
     type Output = V;
 
     #[inline]
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.base[index].1
+    fn index(&self, i: usize) -> &Self::Output {
+        &self.base.index(i).1
     }
 }
 
 impl<K, V> IndexMut<usize> for VecBTreeMap<K, V> {
     #[inline]
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.base[index].1
+    fn index_mut(&mut self, i: usize) -> &mut Self::Output {
+        &mut self.base.index_mut(i).1
     }
 }
